5117c9ef32715211fc294ec6ae71cc4f0c0ac1e8
Marcelo <celoguimaraes@gmail.com>
2017-10-24 00:33:39 -0200

    Personalizando mensagens de erro

--- src/main/java/br/com/guimasoftware/exceptionhandler/BusinessException.java
+++ src/main/java/br/com/guimasoftware/exceptionhandler/BusinessException.java
@@ -1,9 +1,19 @@
 package br.com.guimasoftware.exceptionhandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -11,15 +21,60 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class BusinessException extends ResponseEntityExceptionHandler {
+	@Autowired
+	private MessageSource messageSource;
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-				
-		return handleExceptionInternal(ex, "Mensagem inválida", headers, HttpStatus.BAD_REQUEST, request);
-		
-		
-		
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.getCause().toString();
+		List<BusinessError> errors = Arrays.asList(new BusinessError(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, errors, headers, status, request);
 	}
 	
-}
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) { 
+		List<BusinessError> errors = errosListBuilder(ex.getBindingResult());
+		return handleExceptionInternal(ex, errors, headers, status, request);
+	}
+	
+	private List<BusinessError> errosListBuilder(BindingResult bindingResult){
+		List<BusinessError> errors = new ArrayList<>();
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+			errors.add(new BusinessError(mensagemUsuario, mensagemDesenvolvedor));
+		}
+		return errors;
+	}
+
+	public static class BusinessError {
+
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+
+		public BusinessError(String mensagemUsuario, String mensagemDesenvolvedor) {
+			super();
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+
+		public void setMensagemUsuario(String mensagemUsuario) {
+			this.mensagemUsuario = mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+
+		public void setMensagemDesenvolvedor(String mensagemDesenvolvedor) {
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+	}
+} 
