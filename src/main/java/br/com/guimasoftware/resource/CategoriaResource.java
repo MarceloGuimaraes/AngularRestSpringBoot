371410f53cf5c7adc5287aa351ad8ecbc9d8dc8e
MarceloGuimaraes <celoguimaraes@gmail.com>
2017-11-20 18:54:59 -0200

    Usando eventos para adicionar header Location

--- src/main/java/br/com/guimasoftware/resource/CategoriaResource.java
+++ src/main/java/br/com/guimasoftware/resource/CategoriaResource.java
@@ -1,24 +1,22 @@
 package br.com.guimasoftware.resource;
 
-import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.guimasoftware.event.RecursoCriadoEvent;
 import br.com.guimasoftware.model.Categoria;
 import br.com.guimasoftware.repository.CategoriaRepository;
 
@@ -28,6 +26,9 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public ResponseEntity<?> listar() {
@@ -55,11 +56,16 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		/* Codigo comentado para utilização de event Listener 
+		 * 
+		 * URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		 *	response.setHeader("Location", uri.toASCIIString());
+		 * return ResponseEntity.created(uri).body(categoriaSalva);
+		 * */
 		
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
