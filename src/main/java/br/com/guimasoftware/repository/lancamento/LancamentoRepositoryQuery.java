cba44e04f047340609c6439975f9d9c08f61a157
MarceloGuimaraes <celoguimaraes@gmail.com>
2018-03-02 00:28:43 -0300

    Implementando a paginação de lançamentos

--- src/main/java/br/com/guimasoftware/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/br/com/guimasoftware/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package br.com.guimasoftware.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.guimasoftware.model.Lancamento;
 import br.com.guimasoftware.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
 }
